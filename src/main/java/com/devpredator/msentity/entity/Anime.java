7fece1cdab8397c760600aac4bad9c609231a7e1
di3gokanon89 <diego.paniagua.kanon89@gmail.com>
2021-11-06 18:02:26 -0600

    Proyecto Entity - Mockito Section - Add @NoArgsConstructor and @AllArgsConstructor to create entities with ArgsConstructor and Default Constructor

--- src/main/java/com/devpredator/msentity/entity/Anime.java
+++ src/main/java/com/devpredator/msentity/entity/Anime.java
@@ -15,7 +15,10 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Entity that represents a table in the database.
@@ -25,24 +28,30 @@ import lombok.Data;
  * @version v1.0 21/07/2021
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "anime")
 public class Anime {
 
+	@ApiModelProperty(value = "The id of the anime (Autoincrement)", name = "id", required = true, example = "1")
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
 	
+	@ApiModelProperty(value = "The name of the anime", name = "nombre", required = true, example = "Dragon Ball Z")
 	@NotNull
 	@NotBlank(message = "The name is required")
 	@Size(min = 3, max = 30, message = "The lenght of the name is to less or to long")
 	@Column(name = "nombre")
 	private String nombre;
 	
+	@ApiModelProperty(value = "The year of the anime", name = "anio", example = "2021, Format: yyyy")
 	@Column(name = "anio")
 	private int anio;
 	
+	@ApiModelProperty(value = "The date of the anime", name = "fechaCreacion", example = "2021-06-02'T'00:00:00")
 	@Column(name = "fecha_creacion")
 	private LocalDateTime fechaCreacion;
 
--- src/main/java/com/devpredator/msentity/entity/Personaje.java
+++ src/main/java/com/devpredator/msentity/entity/Personaje.java
@@ -14,26 +14,34 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author DevPredator
  *
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "personaje")
 public class Personaje {
 
+	@ApiModelProperty(value = "The id of the personaje (Autoincrement)", name = "id", required = true, example = "1")
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
 	
+	@ApiModelProperty(value = "The name of the personaje", name = "nombre", required = true, example = "Goku")
 	@NotNull
 	@NotEmpty(message = "The name is required")
 	private String nombre;
-	
+
+	@ApiModelProperty(value = "The object anime", name = "anime", required = true, example = "Dragon Ball Z")
 	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "anime_id")
